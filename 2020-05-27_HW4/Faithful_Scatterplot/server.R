c35a030fb25a9dcd29970ba4dd87bc71e08ea877
NaeemCho <naimul.chowdhury15@ncf.edu>
2020-05-27 23:52:50 -0400

    Added labels to the graph

--- 2020-05-27_HW4/Faithful_Scatterplot/server.R
+++ 2020-05-27_HW4/Faithful_Scatterplot/server.R
@@ -4,6 +4,10 @@ library(ggplot2)
 shinyServer(function(input, output, session) {
     output$scatterPlot <- renderPlot({
         p <- ggplot(faithful, aes(x = waiting, y = eruptions))+
+            labs(title = "Eruptions at Old Faithful",
+                 subtitle="An Interactive Scatterplot",
+                 x = "Time in Seconds", 
+                 y = "Number of Eruptions")+
             geom_point(shape = as.numeric(input$shape), color = input$color)
         return(p)
     })
