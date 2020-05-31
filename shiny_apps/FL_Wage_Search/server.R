bec8e2b08659b422239984bfa32a1a25087182fe
NaeemCho <naimul.chowdhury15@ncf.edu>
2020-05-31 11:11:07 -0400

    added a call to tidyverse package

--- shiny_apps/FL_Wage_Search/server.R
+++ shiny_apps/FL_Wage_Search/server.R
@@ -1,15 +1,11 @@
 library(shiny)
 library(DT)
 library(readr)
+library(tidyverse)
 
 df <- read_csv('./data/FL2018Wages.csv') 
 
 shinyServer(function(input, output, session) {
-  
-  output$wagedata <- renderDT({
-    df <- datatable(df,  filter = 'top')
-    return(df)
-  })
   
   output$endresult <- renderText({
     hours <- input$hours
@@ -17,5 +13,16 @@ shinyServer(function(input, output, session) {
     earnings <- hours * wages
     print(c('The total earnings of working ', hours, ' hours at $', wages, ' dollars per hour is $', earnings, 'before tax.'))
   })
+
+  output$wagedata <- renderDT({
+    wagetype = input$wagetype
+    df <- df %>% 
+      select(Code, Title, wagetype) %>% 
+      mutate('Earnings by No. of Hrs' = input$hours * as.numeric(wagetype))
+    df <- datatable(df,  filter = 'top')
+    return(df)
+  })
+  
+
   
 })
