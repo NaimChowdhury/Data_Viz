cddbe04a7a571ff359486117fc246ba0da9a57a4
NaeemCho <naimul.chowdhury15@ncf.edu>
2020-06-10 00:14:13 -0400

    3.2.3 Numeric Inputs

--- 2020-03-10 HW6/mastering_shiny_3/mastering_shiny_3_practice/app.R
+++ 2020-03-10 HW6/mastering_shiny_3/mastering_shiny_3_practice/app.R
@@ -10,9 +10,9 @@
 library(shiny)
 
 ui <- fluidPage(
-  textInput("name", "What's your name?"),
-  passwordInput("password", "What's your password?"),
-  textAreaInput("story", "Tell me about yourself", rows = 3)
+  numericInput("num", "Number one", value = 0, min = 0, max = 100),
+  sliderInput("num2", "Number two", value = 50, min = 0, max = 100),
+  sliderInput("rng", "Range", value = c(10, 20), min = 0, max = 100)
 )
 
 server <- function(input, output, session) {
