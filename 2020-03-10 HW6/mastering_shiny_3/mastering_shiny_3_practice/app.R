787193c1d1cf008c5e84886dec0cc426ba0dfddd
NaeemCho <naimul.chowdhury15@ncf.edu>
2020-06-10 00:21:30 -0400

    3.2.6 file uploads

--- 2020-03-10 HW6/mastering_shiny_3/mastering_shiny_3_practice/app.R
+++ 2020-03-10 HW6/mastering_shiny_3/mastering_shiny_3_practice/app.R
@@ -9,11 +9,8 @@
 
 library(shiny)
 
-animals <- c("dog", "cat", "mouse", "bird", "other", "I hate animals")
-
 ui <- fluidPage(
-  selectInput("state", "What's your favourite state?", state.name),
-  radioButtons("animal", "What's your favourite animal?", animals)
+  fileInput("upload", NULL)
 )
 
 server <- function(input, output, session) {
