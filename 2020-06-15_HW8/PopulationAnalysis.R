bb701e1cae7f55d99b68fc907995b003d05c6b41
NaeemCho <naimul.chowdhury15@ncf.edu>
2020-06-18 13:24:13 -0400

    Changed all of the text in the population columns to lower-case

--- 2020-06-15_HW8/PopulationAnalysis.R
+++ 2020-06-15_HW8/PopulationAnalysis.R
@@ -2,6 +2,18 @@ mygitpath <- 'https://raw.githubusercontent.com/NaimChowdhury/Data_Viz/master/20
 library(tidyverse)
 # install.packages('maps')
 library(maps)
+
+# loading dataset through hyperlink
 population <- read_csv(mygitpath)
 
+# the package already has a set of counties
 counties <- map_data("county")
+
+population$Area_Name = str_replace_all(population$Area_Name,"[^[:graph:]]", " ") 
+
+population_sub <- population %>% 
+  mutate(county = tolower(Area_Name)) %>% 
+  mutate(state = tolower(State)) %>% 
+  select(state, county)
+
+
