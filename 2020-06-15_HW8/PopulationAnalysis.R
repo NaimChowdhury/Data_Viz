06f7c0c62b376fbe0b6de549609a021755a7616b
NaeemCho <naimul.chowdhury15@ncf.edu>
2020-06-17 22:14:26 -0400

    testing maps package

--- 2020-06-15_HW8/PopulationAnalysis.R
+++ 2020-06-15_HW8/PopulationAnalysis.R
@@ -1,4 +1,7 @@
 mygitpath <- 'https://raw.githubusercontent.com/NaimChowdhury/Data_Viz/master/2020-06-15_HW8/PopulationEstimates.csv'
 library(tidyverse)
+# install.packages('maps')
+library(maps)
 population <- read_csv(mygitpath)
 
+counties <- map_data("county")
