9f46c834d810ab760d24228977518b6b7dbd4d9e
NaimChowdhury <naimul.chowdhury15@ncf.edu>
2020-07-16 15:56:34 -0400

    Initial setup for basic output.

--- CLSAS_Project/CLSAS_Dashboard/ui.R
+++ CLSAS_Project/CLSAS_Dashboard/ui.R
@@ -12,24 +12,34 @@ library(shinydashboard)
 
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+dashboardPage(
+    dashboardHeader(title = "CLSAS Membership"),
+    dashboardSidebar(
+        sliderInput("size", "Size of Points:", min=0.2, max=5, value=2)
+    ),
+    dashboardBody(
+        # Boxes need to be put in a row (or column)
+        fluidRow(
+            box(width=6, 
+                status="info", 
+                title="Plot",
+                solidHeader = TRUE,
+                plotOutput("myplot")
+            ),
+            box(width=6, 
+                status="warning", 
+                title = "Membership Data",
+                solidHeader = TRUE, 
+                collapsible = TRUE, 
+                footer="Read Remotely from File",
+                tableOutput("mydata")
+            )
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
+        ## Add some more info boxes
+        fluidRow(
+            valueBoxOutput(width=4, "nrows"),
+            infoBoxOutput(width=6, "ncol")
         )
     )
-))
+)
+
